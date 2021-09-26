5fe4be2b0a3d432cb74371b50bc01e0558c6199e
KouOsada <kouosada.0930@gmail.com>
2021-09-26 16:36:54 +0900

    [Add]非表示

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 $(function(){
+  // アニメーション
   // $('.box1').slideDown();
   // $('.box1').slideUp();
 
-  $('.box1').show();
-  $('.box1').css({'background-color':'#0000FF'});
+  // 要素の表示・非表示
+  // $('.box1').show();
+  // $('.box1').css({'background-color':'#0000FF'});
+  $('.box1').hide();
 });
