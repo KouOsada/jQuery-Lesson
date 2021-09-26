e5419276ca8bcb212b8c806bc2a24868d42cc6a7
KouOsada <kouosada.0930@gmail.com>
2021-09-26 16:45:05 +0900

    [Finish]JS-5

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-$(function(){
+// $(function(){
   // アニメーション
   // $('.box1').slideDown();
   // $('.box1').slideUp();
@@ -6,5 +6,16 @@ $(function(){
   // 要素の表示・非表示
   // $('.box1').show();
   // $('.box1').css({'background-color':'#0000FF'});
-  $('.box1').hide();
+  // $('.box1').hide();
+// });
+
+// 確認問題
+$(function (){
+  $('.box1').slideDown(3000,function (){
+    $('.box1').css({
+      'background-color': '#0000FF',
+      'width': '200px',
+      'height': '100px'
+    }).slideUp(3000);
+  });
 });
