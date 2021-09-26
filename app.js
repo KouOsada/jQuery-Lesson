0abae48dfc59f1fc43315c7b08119d5c190b38ab
KouOsada <kouosada.0930@gmail.com>
2021-09-26 16:51:43 +0900

    [Start]JS-6

--- app.js
+++ app.js
@@ -10,12 +10,18 @@
 // });
 
 // 確認問題
-$(function (){
-  $('.box1').slideDown(3000,function (){
-    $('.box1').css({
-      'background-color': '#0000FF',
-      'width': '200px',
-      'height': '100px'
-    }).slideUp(3000);
+// $(function (){
+//   $('.box1').slideDown(3000,function (){
+//     $('.box1').css({
+//       'background-color': '#0000FF',
+//       'width': '200px',
+//       'height': '100px'
+//     }).slideUp(3000);
+//   });
+// });
+
+$(function(){
+  $('.box1').mouseover(function(){
+    $('.box1').css({'background-color':'#0000FF'})
   });
 });
